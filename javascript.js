f2dd36985082c7c5152d1abec8b78db8f1215138
David Johnson <david_karl_johnson@hotmail.com>
2025-04-21 21:13:06 +0200

    Added pop-up for email address icon

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function showEmailPopup() {
+    const emailPopup = document.querySelector('.email-popup');
+    emailPopup.style.visibility = 'visible';
+}
+
+function hideEmailPopup() {
+    const emailPopup = document.querySelector('.email-popup');
+    emailPopup.style.visibility = 'hidden';
+}
