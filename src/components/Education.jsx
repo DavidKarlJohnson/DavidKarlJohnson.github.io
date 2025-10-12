e3f608e6f944e08b9db4c579f45f3bc01e957b9d
David Johnson <david_karl_johnson@hotmail.com>
2025-10-12 18:56:15 +0200

    Add sections for different disciplines for 'Education' component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,18 +6,52 @@ export default function Education({ courses }) {
     return (
         <section className="education">
             <h2>Utbildning</h2>
+            <h3>Datavetenskap</h3>
             {
-                courses.map(course => 
-                    <Course 
-                    key={course.id}
-                    name={course.name}
-                    desc={course.desc}
-                    hp={course.hp}
-                    university={course.university}
-                    link={course.link}
-                    toolsArr={course.toolsUsed}
-                    />
-                )
+                courses
+                    .filter(course => course.discipline.localeCompare('computer science') === 0)
+                    .map(course => (
+                        <Course 
+                            key={course.id}
+                            name={course.name}
+                            desc={course.desc}
+                            hp={course.hp}
+                            university={course.university}
+                            link={course.link}
+                            toolsArr={course.toolsUsed}
+                        />
+                    ))
+            }
+            <h3>Matematik</h3>
+            {   courses
+                    .filter(course => course.discipline.localeCompare('math') === 0)
+                    .map(course => (
+                        <Course 
+                            key={course.id}
+                            name={course.name}
+                            desc={course.desc}
+                            hp={course.hp}
+                            university={course.university}
+                            link={course.link}
+                            toolsArr={course.toolsUsed}
+                        />
+                    ))
+            }
+            <h3>Övrigt</h3>
+            {   courses
+                    .filter(course => course.discipline.localeCompare('math') !== 0 &&
+                                      course.discipline.localeCompare('computer science') !== 0 )
+                    .map(course => (
+                        <Course 
+                            key={course.id}
+                            name={course.name}
+                            desc={course.desc}
+                            hp={course.hp}
+                            university={course.university}
+                            link={course.link}
+                            toolsArr={course.toolsUsed}
+                        />
+                    ))
             }
         </section>
     )
