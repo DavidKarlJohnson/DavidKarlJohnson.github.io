d0493616168e0299a15176580012d6280a89a480
David Johnson <david_karl_johnson@hotmail.com>
2025-10-17 18:54:03 +0200

    Add 'scrollIntoView' functionality to the nav buttons
    Clicking the buttons in the nav will now scroll that section into view.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,17 @@
 import '../styles/Navigation.css'
 
 export default function Navigation() {
+
+    function scrollTo(elementSelector) {
+        document.querySelector(elementSelector).scrollIntoView(true);
+    }
+
     return (
         <nav className="navigation">
-            <button>Om mig</button>
-            <button>Utbildning</button>
-            <button>Projekt</button>
-            <button>Kontakt</button>
+            <button onClick={() => scrollTo('.profile')}>Om mig</button>
+            <button onClick={() => scrollTo('.education')}>Utbildning</button>
+            <button onClick={() => scrollTo('.projects')}>Projekt</button>
+            <button onClick={() => scrollTo('.contact-group')}>Kontakt</button>
         </nav>
     )
 }
