b5e9074bd349e7359d2ae7ff7edade290fc23355
David Johnson <david_karl_johnson@hotmail.com>
2025-10-19 20:15:12 +0200

    Add logos for 'Linköpings Universitet', 'Högskolan i Gävle' and 'Högskolan i Borås'

--- src/components/UniversityImg.jsx
+++ src/components/UniversityImg.jsx
@@ -1,6 +1,11 @@
 import '../styles/UniversityImg.css';
 import uuLogo from '../assets/uppsala-universitet-logo.svg';
 import guLogo from '../assets/goteborgs-universitet-logo.svg';
+import luLogo from '../assets/linkopings-universitet-logo.svg';
+import hibLogo from '../assets/hogskolan-i-boras-logo.svg';
+import higLogo from '../assets/hogskolan-i-gavle-logo.svg';
+
+
 
 
 export default function UniversityImg({name}) {
@@ -10,10 +15,18 @@ export default function UniversityImg({name}) {
         case 'uppsala universitet':
             imgSrc = uuLogo;
             break;
+        case 'linköpings universitet':
+            imgSrc = luLogo;
+            break;
         case 'göteborgs universitet':
             imgSrc = guLogo;
             break;
-  
+        case 'högskolan i borås':
+            imgSrc = hibLogo;
+            break;
+        case 'högskolan i gävle':
+            imgSrc = higLogo;
+            break;
         default:
             imgSrc = '';
             break;
