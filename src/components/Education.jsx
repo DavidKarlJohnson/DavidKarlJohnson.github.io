df500cfae84765202c19a165e8f304de1d1e5d37
David Johnson <david_karl_johnson@hotmail.com>
2025-10-28 21:39:42 +0100

    Add media queries and dynamic sizing to Education
    The Education component will now size properly when shrinking the browser
    horizontally.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -45,7 +45,8 @@ export default function Education({ courses }) {
                         <span className='university-date'>2023 - 2026</span>
                     </div>
                 </div>
-                <p>Span</p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div className='education-category-header' onClick={(e) => toggleCourseVisibility(e, '#computer-science')}>
                 <h3>Datavetenskap</h3>
